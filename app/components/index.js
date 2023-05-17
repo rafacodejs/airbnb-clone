daa87c84e08fff1095a2a9377a51292e1f446566
Rafael Malpica <rafacodejs@gmail.com>
2023-05-17 12:10:35 -0400

    Setup project and Navbar component

---
+++ app/components/index.js
@@ -0,0 +1,7 @@
+import { Navbar } from './navbar/Navbar.tsx';
+import { Container } from './Container.tsx';
+import { Logo } from './navbar/Logo.tsx';
+import { Search } from './navbar/Search.tsx';
+import { UserMenu } from './navbar/UserMenu.tsx';
+
+export { Navbar, Container, Logo, Search, UserMenu };
